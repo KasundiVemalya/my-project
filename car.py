81549e756b662a4afcb6b79c6b94f9ec11c392f0
Kasundi Vemalya Sudusinghe <kasundivemalya29@gmail.com>
2024-05-18 01:31:59 +0530

    Added an empty line, so that the outputs will be printed in a new line.

--- car.py
+++ car.py
@@ -6,7 +6,7 @@ class Car:
         self.year = year
 
     def display_info(self):
-        return f"{self.year} {self.make} {self.model}"
+        return f"\n{self.year} {self.make} {self.model}"
 
     def start_engine(self):
         return "The car's engine is now running."
