f13c3b6013069ab07bf9d90caa0271c9e6c02f84
Kasundi Vemalya Sudusinghe <kasundivemalya29@gmail.com>
2024-05-18 01:26:07 +0530

    Added a few functions to the Car class.

--- car.py
+++ car.py
@@ -7,3 +7,9 @@ class Car:
 
     def display_info(self):
         return f"{self.year} {self.make} {self.model}"
+
+    def start_engine(self):
+        return "The car's engine is now running."
+
+    def stop_engine(self):
+        return "The car's engine is now off."
