566f7ccc18dc3468c7e437bee4db4f61ea5f701b
DNWijendra <dinithiw01@gmail.com>
2024-05-18 13:42:05 +0530

    Created a class called Truck.

--- truck.py
+++ truck.py
@@ -1,1 +1,15 @@
+# truck.py
+class Truck:
+    def __init__(self, make, model, capacity):
+        self.make = make
+        self.model = model
+        self.capacity = capacity
 
+    def display_info(self):
+        return f"{self.make} {self.model} with a capacity of {self.capacity} tons"
+
+    def load_cargo(self):
+        return "The truck is loading cargo."
+
+    def unload_cargo(self):
+        return "The truck is unloading cargo."
